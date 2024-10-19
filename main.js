147f55c9304508d6cf2dad9250bf4968836ef714
seungsu-K <kss095@naver.com>
2024-10-20 02:47:29 +0900

    게임보드
    - 지뢰 랜덤 배정 함수

--- main.js
+++ main.js
@@ -1,19 +1,43 @@
 import '/style.css';
 
 const boardSize = 9;
-const mines = 10;
+const rows = 9;
+const cols = 9;
+const minesCount = 10;
 const board = document.querySelector('#board');
 
 function renderBoard() {
-  for (let row = 0; row < boardSize; row++) {
-    for (let col = 0; col < boardSize; col++) {
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
       const cell = document.createElement('div');
       cell.classList.add('cell');
       cell.dataset.row = row;
       cell.dataset.col = col;
+      cell.addEventListener('click', () => console.log(mines[row][col]));
       board.appendChild(cell);
     }
   }
 }
 
 renderBoard();
+
+let mines = new Array(rows)
+  .fill(null)
+  .map(() => new Array(cols).fill(null).map(() => false));
+
+console.log(mines);
+
+function setMines() {
+  let setMines = 0;
+
+  while (setMines < minesCount) {
+    const row = Math.floor(Math.random() * rows);
+    const col = Math.floor(Math.random() * cols);
+    if (!mines[row][col]) {
+      mines[row][col] = true;
+      setMines++;
+    }
+  }
+}
+
+setMines();
