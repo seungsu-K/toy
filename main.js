04d8d4f8fffe93aeb9b658b80fa5004deb6e917b
seungsu-K <kss095@naver.com>
2024-10-20 03:49:36 +0900

    게임보드
    - 셀 클릭 함수 (지뢰만 확인)
    
    재시작 버튼
    
    - 마크업
    - 지뢰 리셋 함수
    - 게임 재시작 함수

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ import '/style.css';
 const boardSize = 9;
 const rows = 9;
 const cols = 9;
+
 const minesCount = 10;
+
 const board = document.querySelector('#board');
+
+let gameOver = false;
 
 function renderBoard() {
   for (let row = 0; row < rows; row++) {
@@ -13,7 +17,7 @@ function renderBoard() {
       cell.classList.add('cell');
       cell.dataset.row = row;
       cell.dataset.col = col;
-      cell.addEventListener('click', () => console.log(mines[row][col]));
+      cell.addEventListener('click', clickCell);
       board.appendChild(cell);
     }
   }
@@ -24,8 +28,6 @@ renderBoard();
 let mines = new Array(rows)
   .fill(null)
   .map(() => new Array(cols).fill(null).map(() => false));
-
-console.log(mines);
 
 function setMines() {
   let setMines = 0;
@@ -38,6 +40,45 @@ function setMines() {
       setMines++;
     }
   }
+
+  return mines;
 }
 
 setMines();
+
+function clickCell() {
+  if (gameOver || this.classList.contains('is-opened')) return;
+
+  let cell = this;
+  cell.classList.add('is-opened');
+
+  if (mines[cell.dataset.row][cell.dataset.col]) {
+    cell.classList.add('mine');
+    alert('Game Over');
+    gameOver = true;
+    return;
+  }
+}
+
+function resetMines() {
+  mines = new Array(rows)
+    .fill(null)
+    .map(() => new Array(cols).fill(null).map(() => false));
+
+  setMines();
+}
+
+function restartGame() {
+  gameOver = false;
+  const cells = document.querySelectorAll('.is-opened');
+  cells.forEach((cell) => cell.classList.remove('is-opened'));
+
+  const mine = document.querySelector('.mine');
+  mine.classList.remove('mine');
+
+  resetMines();
+}
+
+const restartButton = document.querySelector('.button_restart');
+
+restartButton.addEventListener('click', restartGame);
